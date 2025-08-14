b67e3acea0cb93e3bb65c76ef971412f0c2303ca
Alexchua <S10259411@connect.np.edu.sg>
2025-08-14 09:42:18 +0800

    menu

--- MenuItem.cs
+++ MenuItem.cs
@@ -1,29 +1,28 @@
-﻿namespace SDP_assignment
+﻿using System;
+
+public class MenuItem : MenuComponent
 {
-    public class MenuItem : MenuComponent
+    private string _name;
+    private string _description;
+    private decimal _price;
+    private int _userId;
+
+    public MenuItem(string name, string description, decimal price, int userId)
     {
-        private readonly string _name;
-        private readonly string _description;
-        private readonly decimal _price;
-        private readonly int _userId;
+        _name = name;
+        _description = description;
+        _price = price;
+        _userId = userId;
+    }
 
-        public MenuItem(string name, string description, decimal price, int userId)
-        {
-            _name = name;
-            _description = description;
-            _price = price;
-            _userId = userId;
-        }
+    public override string Name => _name;
+    public override string Description => _description;
+    public override decimal Price => _price;
 
-        public override string Name => _name;
-        public override string Description => _description;
-        public override decimal Price => _price;
-
-        public override void Print()
-        {
-            Console.Write($"  {_name}");
-            Console.WriteLine($": ${_price:N2}");
-            Console.WriteLine($"  -- {_description}");
-        }
+    public override void Print()
+    {
+        Console.Write($"  {_name}");
+        Console.WriteLine($": ${_price:N2}");
+        Console.WriteLine($"  -- {_description}");
     }
 }
