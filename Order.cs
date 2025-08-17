037c6b1eb61b16227df106941936e533af4982d3
Alexchua <S10259411@connect.np.edu.sg>
2025-08-17 21:39:17 +0800

    changes

--- Order.cs
+++ Order.cs
@@ -1,52 +1,21 @@
-using System.Collections.Generic;
-using System;
+using SDP_assignment;
 
-namespace SDP_assignment
+public class Order
 {
+    public string Id { get; } = Guid.NewGuid().ToString();
+    public string CustomerName { get; set; }
+    public DateTime OrderDate { get; } = DateTime.Now;
+    public List<MenuItem> Items { get; set; } = new List<MenuItem>();
+    public decimal TotalAmount => Items.Sum(item => item.Price);
+    private PaymentStrategy paymentStrategy;
 
-    public class Order
+    public void SetPaymentStrategy(PaymentStrategy strategy)
     {
-        public string Id { get; } = Guid.NewGuid().ToString();
-        public string CustomerName { get; set; }
-        public DateTime OrderDate { get; } = DateTime.Now;
-        public List<MenuItem> Items { get; set; } = new List<MenuItem>();
-        public decimal TotalAmount => Items.Sum(item => item.Price);
-        public OrderState State { get; set; } = new PendingPaymentState();
-        private PaymentStrategy paymentStrategy;
-        private List<OrderObserver> observers = new List<OrderObserver>();
+        paymentStrategy = strategy;
+    }
 
-        public void SetPaymentStrategy(PaymentStrategy strategy)
-        {
-            paymentStrategy = strategy;
-        }
-
-        public void AttachObserver(OrderObserver observer)
-        {
-            observers.Add(observer);
-        }
-
-        public void ProcessPayment()
-        {
-            if (paymentStrategy.ProcessPayment(TotalAmount))
-            {
-                State = new PaidState();
-                NotifyObservers();
-            }
-        }
-
-        public void RestaurantReject()
-        {
-            State = new RejectedState();
-            paymentStrategy.ProcessRefund(TotalAmount);
-            NotifyObservers();
-        }
-
-        private void NotifyObservers()
-        {
-            foreach (var observer in observers)
-            {
-                observer.Update(this);
-            }
-        }
+    public void ProcessPayment()
+    {
+        paymentStrategy.ProcessPayment(TotalAmount);
     }
 }
